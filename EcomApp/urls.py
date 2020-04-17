6d670b921692825be39bf81fedaa4d10dd84b5ae
Naraesh <naraeshkumar96@gmail.com>
2020-04-17 15:32:19 +0530

    updated

--- EcomApp/urls.py
+++ EcomApp/urls.py
@@ -2,14 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('',views.index,name='home'),
-   path('blog/',views.blog,name='blog'),
-   path('blog-detail/',views.blog_detail,name='blog_detail'),
-   path('shop/',views.shop,name='shop'),
-   path('contact/',views.contact,name='contact'),
-   path('shopping/',views.shopping,name='shopping'),
-   path('faq/',views.faq,name='faq'),
-   path('checkout/',views.checkout,name='checkout'),
-   path('register/',views.register,name='register'),
-    path('login/',views.log_in,name='login')
-]
+  path('',views.index)
+  ]
--- EcomApp/views.py
+++ EcomApp/views.py
@@ -1,26 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import View
 
-# Create your views here.
 def index(request):
-    return render(request,'index.html',{'home':'active'})
-def blog(request):
-    return render(request,'blog/blog.html',{'blog':'active'})
-def blog_detail(request):
-    return render(request,'blog/blog_detail.html',{'blog':'active'})
-def shop(request):
-    return render(request,'shop/shop.html',{'shop':'active'})
-def contact(request):
-    return render(request,'contact/contact.html',{'contact':'active'})
-def shopping(request):
-    return render(request,'pages/shopping-cart.html',{'shopping':'active'})
-def faq(request):
-    return render(request,'pages/FAQ.html',{'faq':'active'})
-def checkout(request):
-    return render(request,'pages/checkout.html',{'checkout':'active'})
-def register(request):
-    return render(request,'registration/register.html')
-
-def log_in(request):
-    return render(request,'registration/login.html')
+    return render(request,'base/index.html')
 
